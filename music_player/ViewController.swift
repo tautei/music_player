da577c7b1b8ba83c4a00892db83c25301bd6c6ef
monet <tautei.net@gmail.com>
2020-03-24 23:51:25 +0800

    Initial Commit

---
+++ music_player/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  music_player
+//
+//  Created by monet-macbook-pro on 2020/3/24.
+//  Copyright © 2020 monet-macbook-pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ music_playerTests/music_playerTests.swift
@@ -0,0 +1,34 @@
+//
+//  music_playerTests.swift
+//  music_playerTests
+//
+//  Created by monet-macbook-pro on 2020/3/24.
+//  Copyright © 2020 monet-macbook-pro. All rights reserved.
+//
+
+import XCTest
+@testable import music_player
+
+class music_playerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
